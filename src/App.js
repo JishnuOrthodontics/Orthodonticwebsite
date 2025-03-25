b741a192ad972285a430e5ab4503e20695473122
Dr Jishnu S <jishnu262@gmail.com>
2025-03-25 14:30:45 +0530

    Protected Route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 
 function App() {
   return (
@@ -20,8 +22,8 @@ function App() {
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<ProtectedRoute> <About /> </ProtectedRoute>} />
+        <Route path="/contact" element={<ProtectedRoute> <Contact /> </ProtectedRoute>} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
